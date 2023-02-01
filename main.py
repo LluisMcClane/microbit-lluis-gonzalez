cb5a324e2f64ac0a063cd96a25e3c92fd3b2cd1b
LluisMcClane <98152023+LluisMcClane@users.noreply.github.com>
2023-02-01 19:04:22 +0100

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,8 @@
+base_number = 0 #this is the the base number which will be used in our loops 
+bsclicked = 0 #number of times the button B is clicked
+astimes = 0 #number of times the button A is clicked to control which numbers we will display
+
+def user_interact(interval: number)
+{
+    
+}
---
+++ main.ts
@@ -0,0 +1,5 @@
+let base_number = 0
+// this is the the base number which will be used in our loops 
+let bsclicked = 0
+// number of times the button B is clicked
+let astimes = 0
